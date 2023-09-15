465e1da859d324fcc4164ef502dbf2f4e69a65ee
Felipe Woo <felipe.becerra.woo@gmail.com>
2023-09-15 07:41:53 -0600

    change images interval

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const images = ['img/img1.svg', 'img/img2.svg','img/img3.svg', 'img/img2.svg'];  // Add more image names here
+    const images = ['img/img1.svg', 'img/img2.svg', 'img/img2.svg','img/img3.svg' ,'img/img2.svg','img/img2.svg'];  // Add more image names here
     let index = 0;
 
     setInterval(() => {
         index = (index + 1) % images.length;
         document.getElementById("prendamex-image").src = images[index];
-    }, 15000);  // 15 seconds
+    }, 10000);  // 10 seconds
 });
