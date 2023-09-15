e7b93226e088b96866e265e7a10725587f2862cd
Felipe Woo <felipe.becerra.woo@gmail.com>
2023-09-15 04:59:06 -0600

    update images

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const images = ['img/img1.svg', 'img/img2.svg'];  // Add more image names here
+    const images = ['img/img1.svg', 'img/img2.svg','img/img3.svg', 'img/img2.svg'];  // Add more image names here
     let index = 0;
 
     setInterval(() => {
         index = (index + 1) % images.length;
         document.getElementById("prendamex-image").src = images[index];
-    }, 20000);  // 20 seconds
+    }, 15000);  // 15 seconds
 });
