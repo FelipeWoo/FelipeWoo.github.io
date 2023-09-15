ac47c77b6f0c62d437f5fc8c53992141510e5b76
Felipe Woo <felipe.becerra.woo@gmail.com>
2023-09-15 03:12:24 -0600

    change of image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const images = ['img/img1.jpg', 'img/img2.jpg'];  // Add more image names here
+    const images = ['img/img1.svg', 'img/img2.jpg'];  // Add more image names here
     let index = 0;
 
     setInterval(() => {
         index = (index + 1) % images.length;
-        document.getElementById("rotating-image").src = images[index];
+        document.getElementById("prendamex-image").src = images[index];
     }, 20000);  // 20 seconds
 });
